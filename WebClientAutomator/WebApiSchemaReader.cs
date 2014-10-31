88e4592a7a4ce5fe0a4329f91367370e1c74d467
Paul Patarinski <paulpatarinski@gmail.com>
2014-10-30 20:46:17 -0500

    Formatting

--- WebClientAutomator/WebApiSchemaReader.cs
+++ WebClientAutomator/WebApiSchemaReader.cs
@@ -7,197 +7,199 @@ using WebClientAutomator.Models;
 
 namespace WebClientAutomator
 {
-    public class WebApiSchemaReader
+  public class WebApiSchemaReader
+  {
+    public WebApiModel GetWebApiSchema(Assembly assembly)
     {
-      public WebApiModel GetWebApiSchema(Assembly assembly)
+      var controllers =
+        assembly.GetTypes()
+          .Where(type => type != typeof (ApiController) && !type.Name.Equals("WebApiSchemaController") &&
+                         typeof (ApiController).IsAssignableFrom(type)).ToList();
+
+      var result = new WebApiModel {Controllers = new List<Controller>()};
+
+      foreach (var controller in controllers)
       {
-        var controllers =
-          assembly.GetTypes()
-            .Where(type => type != typeof (ApiController) && !type.Name.Equals("WebApiSchemaController") &&
-                           typeof (ApiController).IsAssignableFrom(type)).ToList();
-
-        var result = new WebApiModel {Controllers = new List<Controller>()};
-
-        foreach (var controller in controllers)
+        var controllerModel = new Controller
         {
-          var controllerModel = new Controller
+          Name = controller.Name,
+          Methods = new List<Method>()
+        };
+
+        foreach (
+          var methodInfo in
+            controller.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly)
+              .Where(x => !x.IsSpecialName)
+              .ToList())
+        {
+          var methodModel = new Method {Name = methodInfo.Name};
+
+          var returnType = methodInfo.ReturnType;
+
+          var methodReturnType = GetMethodReturnType(returnType);
+          methodModel.ReturnType = methodReturnType;
+
+          if (methodReturnType != MethodReturnType.Void)
           {
-            Name = controller.Name,
-            Methods = new List<Method>()
-          };
+            var primitiveType = GetPrimitiveTypeByMethodReturnType(methodReturnType, returnType);
+            methodModel.PrimitiveType = primitiveType;
 
-          foreach (
-            var methodInfo in
-              controller.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly)
-                .Where(x => !x.IsSpecialName)
-                .ToList())
-          {
-            var methodModel = new Method {Name = methodInfo.Name};
-
-            var returnType = methodInfo.ReturnType;
-
-            var methodReturnType = GetMethodReturnType(returnType);
-            methodModel.ReturnType = methodReturnType;
-
-            if (methodReturnType != MethodReturnType.Void)
+            if (primitiveType == null)
             {
-              var primitiveType = GetPrimitiveTypeByMethodReturnType(methodReturnType, returnType);
-              methodModel.PrimitiveType = primitiveType;
-
-              if (primitiveType == null)
+              if (methodReturnType == MethodReturnType.TaskT || methodReturnType == MethodReturnType.IEnumerableT)
               {
-                if (methodReturnType == MethodReturnType.TaskT || methodReturnType == MethodReturnType.IEnumerableT)
-                {
-                  var taskTType = returnType.GenericTypeArguments.First();
+                var taskTType = returnType.GenericTypeArguments.First();
 
-                  if (taskTType.IsPrimitiveType())
-                    methodModel.PrimitiveType = GetPrimitiveType(taskTType.Name);
-                  else if (taskTType.IsHttpResult())
-                    methodModel.PrimitiveType = PrimitiveType.String;
-                  else
-                    methodModel.ComplexType = GetComplexType(taskTType);
-                }
+                if (taskTType.IsPrimitiveType())
+                  methodModel.PrimitiveType = GetPrimitiveType(taskTType.Name);
+                else if (taskTType.IsHttpResult())
+                  methodModel.PrimitiveType = PrimitiveType.String;
                 else
-                {
-                  methodModel.ComplexType = GetComplexType(returnType);
-                }
+                  methodModel.ComplexType = GetComplexType(taskTType);
+              }
+              else
+              {
+                methodModel.ComplexType = GetComplexType(returnType);
               }
             }
-
-            controllerModel.Methods.Add(methodModel);
           }
 
-          result.Controllers.Add(controllerModel);
+          controllerModel.Methods.Add(methodModel);
         }
 
-        return result;
+        result.Controllers.Add(controllerModel);
       }
 
-      private PrimitiveType? GetPrimitiveTypeByMethodReturnType(MethodReturnType methodReturnType, Type returnType)
+      return result;
+    }
+
+    private PrimitiveType? GetPrimitiveTypeByMethodReturnType(MethodReturnType methodReturnType, Type returnType)
+    {
+      switch (methodReturnType)
       {
-        switch (methodReturnType)
+        case MethodReturnType.Primitive:
         {
-          case MethodReturnType.Primitive:
-          {
-            return GetPrimitiveType(returnType.Name);
-          }
-            //This is a special case. You would want to deserialize HTTPResult into a string
-          case MethodReturnType.HttpResult:
-          case MethodReturnType.Task:
-          case MethodReturnType.IEnumerable:
-          {
-            return PrimitiveType.String;
-          }
-          default:
-            return null;
+          return GetPrimitiveType(returnType.Name);
         }
+          //This is a special case. You would want to deserialize HTTPResult into a string
+        case MethodReturnType.HttpResult:
+        case MethodReturnType.Task:
+        case MethodReturnType.IEnumerable:
+        {
+          return PrimitiveType.String;
+        }
+        default:
+          return null;
       }
+    }
 
-      public MethodReturnType GetMethodReturnType(Type returnType)
+    public MethodReturnType GetMethodReturnType(Type returnType)
+    {
+      MethodReturnType methodReturnType;
+
+      if (returnType == typeof (void))
       {
-        MethodReturnType methodReturnType;
-
-        if (returnType == typeof (void))
+        methodReturnType = MethodReturnType.Void;
+      }
+      else if (returnType.IsHttpResult())
+      {
+        methodReturnType = MethodReturnType.HttpResult;
+      }
+      else if (returnType.IsPrimitiveType())
+      {
+        methodReturnType = MethodReturnType.Primitive;
+      }
+      else
+      {
+        if (returnType.IsTask())
         {
-          methodReturnType = MethodReturnType.Void;
+          methodReturnType = returnType.GenericTypeArguments.Any() ? MethodReturnType.TaskT : MethodReturnType.Task;
         }
-        else if (returnType.IsHttpResult())
+        else if (returnType.IsIEnumerable())
         {
-          methodReturnType = MethodReturnType.HttpResult;
-        }
-        else if (returnType.IsPrimitiveType())
-        {
-          methodReturnType = MethodReturnType.Primitive;
+          methodReturnType = returnType.GenericTypeArguments.Any()
+            ? MethodReturnType.IEnumerableT
+            : MethodReturnType.IEnumerable;
         }
         else
         {
-          if (returnType.IsTask())
-          {
-            methodReturnType = returnType.GenericTypeArguments.Any() ? MethodReturnType.TaskT : MethodReturnType.Task;
-          }
-          else if (returnType.IsIEnumerable())
-          {
-            methodReturnType = returnType.GenericTypeArguments.Any() ? MethodReturnType.IEnumerableT : MethodReturnType.IEnumerable;
-          }
-          else
-          {
-            methodReturnType = MethodReturnType.ComplexType;
-          }
+          methodReturnType = MethodReturnType.ComplexType;
         }
-
-        return methodReturnType;
-
       }
 
-      public
-    ComplexType GetComplexType(Type type)
+      return methodReturnType;
+
+    }
+
+    public
+      ComplexType GetComplexType(Type type)
+    {
+      var complexType = new ComplexType {Properties = new List<Property>(), Name = type.Name};
+
+      var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+      foreach (var propertyInfo in properties)
       {
-        var complexType = new ComplexType{Properties = new List<Property>(), Name = type.Name};
+        var property = new Property {Name = propertyInfo.Name};
 
-        var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
-
-        foreach (var propertyInfo in properties)
+        if (propertyInfo.PropertyType.IsPrimitiveType())
         {
-          var property = new Property{Name = propertyInfo.Name};
+          property.PropertyType = PropertyType.Primitive;
+          property.PrimitiveType = GetPrimitiveType(propertyInfo.PropertyType.Name);
+        }
+        else
+        {
+          if (propertyInfo.PropertyType.IsIEnumerable())
+          {
+            property.PropertyType = PropertyType.IEnumerable;
+            var tType = propertyInfo.PropertyType.GenericTypeArguments.FirstOrDefault();
 
-          if (propertyInfo.PropertyType.IsPrimitiveType())
-          {
-            property.PropertyType = PropertyType.Primitive;
-            property.PrimitiveType = GetPrimitiveType(propertyInfo.PropertyType.Name);
-          }
-          else
-          {
-            if (propertyInfo.PropertyType.IsIEnumerable())
+            if (tType != null)
             {
-              property.PropertyType = PropertyType.IEnumerable;
-              var tType = propertyInfo.PropertyType.GenericTypeArguments.FirstOrDefault();
+              property.PropertyType = PropertyType.IEnumerableT;
 
-              if (tType != null)
+              if (tType.IsPrimitiveType())
               {
-                property.PropertyType = PropertyType.IEnumerableT;
-
-                if (tType.IsPrimitiveType())
-                {
-                  property.PrimitiveType = GetPrimitiveType(tType.Name);
-                }
-                else
-                {
-                  property.ComplexType = GetComplexType(tType);
-                }
+                property.PrimitiveType = GetPrimitiveType(tType.Name);
+              }
+              else
+              {
+                property.ComplexType = GetComplexType(tType);
               }
             }
-            else
-            {
-              property.PropertyType = PropertyType.Complex;
-              property.ComplexType = GetComplexType(propertyInfo.PropertyType);
-            }
           }
-
-          complexType.Properties.Add(property);
+          else
+          {
+            property.PropertyType = PropertyType.Complex;
+            property.ComplexType = GetComplexType(propertyInfo.PropertyType);
+          }
         }
 
-        return complexType;
+        complexType.Properties.Add(property);
       }
 
-      private PrimitiveType GetPrimitiveType(string propertyType)
+      return complexType;
+    }
+
+    private PrimitiveType GetPrimitiveType(string propertyType)
+    {
+      switch (propertyType.ToLower())
       {
-        switch (propertyType.ToLower())
-        {
-          case "string":
-            return PrimitiveType.String;
-          case "double":
-            return PrimitiveType.Double; 
-          case "DateTime":
-            return PrimitiveType.DateTime;
-          case "int":
-            return PrimitiveType.Int;
-          case "boolean":
-            return PrimitiveType.Bool;
-          case "char":
-            return PrimitiveType.Char;
-          default:
-            return PrimitiveType.Unsupported;
-        }
+        case "string":
+          return PrimitiveType.String;
+        case "double":
+          return PrimitiveType.Double;
+        case "DateTime":
+          return PrimitiveType.DateTime;
+        case "int":
+          return PrimitiveType.Int;
+        case "boolean":
+          return PrimitiveType.Bool;
+        case "char":
+          return PrimitiveType.Char;
+        default:
+          return PrimitiveType.Unsupported;
       }
     }
+  }
 }

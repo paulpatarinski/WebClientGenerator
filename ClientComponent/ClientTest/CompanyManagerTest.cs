8a3181e750a5820a9d458bf9b727054a08f2f00a
ppatarinski <ppatarinski@paylocity.com>
2015-01-30 09:38:35 -0600

    Adding some tests and the package.config

---
+++ ClientComponent/ClientTest/CompanyManagerTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApiClient;
+
+namespace ClientTest
+{
+  [TestClass]
+  public class CompanyManagerTest
+  {
+    [TestMethod]
+    public async Task GetCompanies_ShouldReturnCompanies()
+    {
+      var companyManager = new CompanyManager();
+
+      var companies = await companyManager.GetCompanies();
+
+      Assert.IsNotNull(companies);
+      Assert.IsTrue(companies.Any());
+    }
+
+    [TestMethod]
+    public async Task GetCompanyByCode_ShouldReturnACompanyMatchingTheCode()
+    {
+      var companyManager = new CompanyManager();
+
+      var company = await companyManager.GetCompanyByCode("2000");
+
+      Assert.IsNotNull(company);
+    }
+
+    [TestMethod]
+    public async Task GetCompanyByQuery_ShouldReturnACompanyMatchingTheQuery()
+    {
+      var companyManager = new CompanyManager();
+
+      var company = await companyManager.GetCompanyByQuery(new CompanyQuery
+      {
+        Code = "2000",
+      });
+
+      Assert.IsNotNull(company);
+    }
+  }
+}
